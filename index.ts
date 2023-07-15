3fdd831ffd877467fd7b89740e2d9e3a2fa7b53c
Prakash Bhattarai <lightvelocity888@gmail.com>
2023-07-15 09:36:44 +0545

    refactor project structure

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import userRouter from "./app/service/user/router";
 
 const app = express();
 
@@ -6,7 +7,9 @@ app.get("/", (req, res) => {
   res.end("Hello, World");
 });
 
+app.use("/user", userRouter);
+
 const port = process.env.PORT || 8000;
 app.listen(port);
 
-console.log(`Server Listening (${port})`)
+console.log(`Server Listening (${port})`);
