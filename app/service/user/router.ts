b4729552435f94575215acd30022352b55273056
Prakash Bhattarai <lightvelocity888@gmail.com>
2023-07-15 12:14:01 +0545

    add async error handler

--- app/service/user/router.ts
+++ app/service/user/router.ts
@@ -1,11 +1,13 @@
 import express from 'express'
-import prisma from '../../../prisma'
+import wrap from 'express-async-handler'
 
 const router = express.Router()
 
-router.get('/', async (req, res) => {
-  const users = await prisma.user.findMany()
-  res.json(users)
-})
+router.get(
+  '/',
+  wrap(async () => {
+    await Promise.reject(1)
+  }),
+)
 
 export default router
